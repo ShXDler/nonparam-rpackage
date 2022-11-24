d21b72d994bcc08020b9bb034a0c8c5e92724e65
shxdler <1614062951@qq.com>
2022-11-24 00:13:40 -0500

    Tests completed

--- tests/testthat/test-McNemar.R
+++ tests/testthat/test-McNemar.R
@@ -1,9 +1,11 @@
 test_that("multiplication works", {
   set.seed(456)
-  x <- matrix(sample(10, 160, replace=TRUE), 4, 40) + 10
-  for(i in 1:40){
+  x <- matrix(sample(10, 80, replace=TRUE), 4, 20) + 10
+  for(i in 1:20){
     t <- matrix(x[, i], 2, 2)
     expect_equal(mcnemar.test(t)$p.value,
                  unname(McNemar.test(t)$pvalue))
+    expect_equal(mcnemar.test(t, correct=FALSE)$p.value,
+                 unname(McNemar.test(t, correct=FALSE)$pvalue))
     }
 })
--- tests/testthat/test-wilcoxon.R
+++ tests/testthat/test-wilcoxon.R
@@ -1,20 +1,24 @@
 test_that("multiplication works", {
   set.seed(456)
-  x <- matrix(rnorm(100), 10, 10)
-  for(i in 1:10)
+  x <- matrix(rnorm(50), 10, 5)
+  for(i in 1:5)
     expect_equal(wilcox.test(x[, i])$p.value, unname(wilcoxon.test(x[, i])$pvalue))
 
   # Paired two-sample test
-  y <- matrix(rnorm(100), 10, 10)
-  for(i in 1:10){
+  y <- matrix(rnorm(50), 10, 5)
+  for(i in 1:5){
     expect_equal(wilcox.test(x[, i], y[, i], paired=TRUE)$p.value,
                  unname(wilcoxon.test(x[, i], y[, i], paired=TRUE)$pvalue))
+    expect_equal(wilcox.test(x[, i], y[, i], paired=TRUE, alternative='less')$p.value,
+                 unname(wilcoxon.test(x[, i], y[, i], paired=TRUE, alternative='less')$pvalue))
+    expect_equal(wilcox.test(x[, i], y[, i], paired=TRUE, alternative='greater')$p.value,
+                 unname(wilcoxon.test(x[, i], y[, i], paired=TRUE, alternative='greater')$pvalue))
     expect_equal(wilcox.test(x[, i] - y[, i])$p.value,
                  unname(wilcoxon.test(x[, i] - y[, i])$pvalue))
   }
 
   # Two-sample test
-  for(i in 1:10){
+  for(i in 1:5){
     expect_equal(wilcox.test(x[, i], y[1:5, i])$p.value,
                  unname(wilcoxon.test(x[, i], y[1:5, i])$pvalue))
   }
